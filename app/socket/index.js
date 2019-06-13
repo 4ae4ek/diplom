07017d316099a988fcc8f671cbf4ecd7506ee498
Nikita Strokin <popka.dj@gmail.com>
2019-06-14 02:01:30 +0300

    Script reload

--- app/socket/index.js
+++ app/socket/index.js
@@ -62,16 +62,7 @@ var init = function(app){
 
 	var server 	= require('http').Server(app);
 	var io 		= require('socket.io')(server);
-     
-     var http, options, proxy, url;
 
-http = require("http");
-
-url = require("url");
-
-proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
-
-var target  = url.parse("mighty-sea-79953.herokuapp.com");
 
 	// Using Redis
 	let port = config.redis.port;
@@ -85,41 +76,6 @@ var target  = url.parse("mighty-sea-79953.herokuapp.com");
 	io.use((socket, next) => {
 		require('../session')(socket.request, {}, next);
 	});
-
-
-options = {
-  hostname: proxy.hostname,
-  port: proxy.port || 80,
-  path: target.href,
-  headers: {
-    "Proxy-Authorization": "Basic " + (new Buffer.from(proxy.auth).toString("base64")),
-    "Host" : target.hostname
-  }
-};
-
-http.get(options, function(res) {
-  res.pipe(process.stdout);
-  return console.log("status code", res.statusCode);
-});
-
-
-var request = require('request');
-
-var options = {
-    proxy: process.env.QUOTAGUARDSTATIC_URL,
-    url: 'https://api.github.com/repos/joyent/node',
-    headers: {
-        'User-Agent': 'node.js'
-    }
-};
-
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(body);
-    }
-}
-
-request(options, callback);
 
 	// Define all Events
 	ioEvents(io);
--- server.js
+++ server.js
@@ -16,6 +16,55 @@ var logger      = require('./app/logger');
 var mailer      = require('./app/mailer');
 
 
+var port = process.env.PORT || 3000;
+
+
+
+
+
+var http, options, proxy, url;
+
+http = require("http");
+
+url = require("url");
+
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target  = url.parse("http://ip.quotaguard.com/");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
+
+
+
+var request = require('request');
+
+var options = {
+    proxy: process.env.QUOTAGUARDSTATIC_URL,
+    url: 'https://api.github.com/repos/joyent/node',
+    headers: {
+        'User-Agent': 'node.js'
+    }
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
 
 
 app.set('views', path.join(__dirname, 'app/views'));
