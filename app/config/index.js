5b84b8424d23eb8ee3eb1c8d6491faf91a646fa7
Nikita Strokin <popka.dj@gmail.com>
2019-06-13 22:56:05 +0300

    Script reload

--- app/config/index.js
+++ app/config/index.js
@@ -7,7 +7,7 @@ var init = function () {
 		var redisPassword 	= redisURI.auth.split(':')[1];
 		return {
 			
-			sessionSecret: process.env.sessionSecret,
+			sessionSecret: process.env.asfjaip,
 			redis: {
 				host: redisURI.hostname,
 				port: redisURI.port,
